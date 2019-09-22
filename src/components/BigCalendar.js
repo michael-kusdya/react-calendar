823eac1a164c96d537f747ecefc105c4e20faa31
michael <michael.kusdya@gmail.com>
2019-09-22 21:39:28 +0700

    refactor, move modal to separate component

--- src/components/BigCalendar.js
+++ src/components/BigCalendar.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
 import CustomToolbar from './Toolbar'
-import Popup from "reactjs-popup";
+import Modal from './Modal'
 import Input from './Input';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 
@@ -11,44 +11,30 @@ const localizer = momentLocalizer(moment); // or globalizeLocalizer
 class BigCalendar extends Component {
 
     state = {
-        name: 'React',
+        slotInfo: '',
         openCreate: false,
-        events: [],
-        title: '',
-        detail: ''
+        openDetail: false,
+        events: []
       };
 
     onSelectEventHandler = (slotInfo) => {
-        this.setState({ openDetail: !this.state.openDetail, slotInfo: slotInfo });
-        console.log(slotInfo)
+        this.setState({ openDetail: true, slotInfo: slotInfo });
     }
 
-    openModal = (slotInfo) => {
-        this.setState({ openCreate: !this.state.openCreate, slotInfo: slotInfo });
-        console.log(slotInfo)
+    onSelectSlotHandler = (slotInfo) => {
+        this.setState({ openCreate: true, slotInfo: slotInfo });
     }
 
-    closeModal = () => {
+    closeModalCreate = () => {
       this.setState({ openCreate: false });
     }
 
-    onTitleChange = (e) => {
-        this.setState({ title: e.target.value})
-    }
+    closeModalDetail = () => {
+        this.setState({ openDetail: false });
+      }
 
-    onDetailChange = (e) => {
-        this.setState({ detail: e.target.value})
-    }
-
-    onSubmit = () => {
-        let event = {
-            title: this.state.title,
-            detail: this.state.detail,  
-            start: this.state.slotInfo.slots[0],
-            end: this.state.slotInfo.slots[0]
-        }
+    onSubmit = (event) => {
         this.setState({ events: [...this.state.events, event], openCreate: false })
-        console.log(this.state.events)
     }
 
     render() {
@@ -58,48 +44,20 @@ class BigCalendar extends Component {
                     popup
                     selectable
                     components={{toolbar: CustomToolbar}}
-                    views={['month']}
                     events={this.state.events}
                     defaultDate={moment().toDate()}
                     onSelectEvent={(slotInfo) => this.onSelectEventHandler(slotInfo)}
-                    onSelectSlot={(slotInfo) => this.openModal(slotInfo)}
+                    onSelectSlot={(slotInfo) => this.onSelectSlotHandler(slotInfo)}
                     localizer={localizer}
                 />
-                <Popup
-                    open={this.state.openCreate}
-                    onClose={this.closeModal}
-                    >
-                    <div className="modal">
-                        <a className="close" onClick={this.closeModal}>
-                            &times;
-                        </a>
-                        <div className="header">Create Event</div>
-                        <div className="content">
-                            {/* <Input onChange={this.titleChange} placeholder="Event Title" />
-                            <Input onChange={this.locationChange} placeholder="Event Location" /> */}
-                            <input type="text" placeholder="Event Title" className="modal_input" onChange={this.onTitleChange} />
-                            <input type="text" placeholder="Event Detail" className="modal_input" onChange={this.onDetailChange} />
-                        </div>
-                        <div className="actions">
-                            <button className="modal_btn" onClick={this.onSubmit}> Save </button>
-                        </div>
-                    </div>
-                </Popup>
-                <Popup
-                    open={this.state.openDetail}
-                    onClose={this.closeModal}
-                    >
-                    <div className="modal">
-                        <a className="close" onClick={this.closeModal}>
-                            &times;
-                        </a>
-                        <div className="header">{this.state.slotInfo ? this.state.slotInfo.title : ''}</div>
-                        <div className="content">
-                            <h4>Date: {this.state.slotInfo ? this.state.slotInfo.start.toLocaleDateString() : ''}</h4>
-                            <h4>Detail: {this.state.slotInfo ? this.state.slotInfo.detail : ''}</h4>
-                        </div>
-                    </div>
-                </Popup>
+                <Modal 
+                    openCreate={this.state.openCreate} 
+                    openDetail={this.state.openDetail} 
+                    slotInfo={this.state.slotInfo} 
+                    closeModalDetail={this.closeModalDetail} 
+                    closeModalCreate={this.closeModalCreate} 
+                    onSubmit={this.onSubmit} 
+                />
             </div>
 
         );
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -4,31 +4,72 @@ import Input from './Input';
 
 class Modal extends Component {
 
-    closeModal = () =>         {
-        this.setState({ open: false });
+    state = {
+        title: '',
+        detail: '',
+    }
+
+    onTitleChange = (e) => {
+        this.setState({ title: e.target.value})
+    }
+
+    onDetailChange = (e) => {
+        this.setState({ detail: e.target.value})
+    }
+
+    onSubmit = () => {
+        let event = {
+            title: this.state.title,
+            detail: this.state.detail,  
+            start: this.props.slotInfo.slots[0],
+            end: this.props.slotInfo.slots[0]
+        }
+        this.props.onSubmit(event)      
     }
 
     render() {
-        return (
-            <Popup
-                open={props.open}
-                onClose={this.closeModal}
-            >
+        if(this.props.openCreate ){
+            return ( 
+                <Popup
+                open={this.props.openCreate}
+                onClose={this.props.closeModalCreate}
+                >
                 <div className="modal">
-                    <a className="close" onClick={this.closeModal}>
+                    <a className="close" onClick={this.props.closeModalCreate}>
                       &times;
                     </a>
-                    <div className="header">Title</div>
+                    <div className="header">Create Event</div>
                     <div className="content">
-                        <Input placeholder="Event Title" />
-                        <Input placeholder="Event Location" />
+                        <input type="text" placeholder="Event Title" className="modal_input" onChange={this.onTitleChange} />
+                        <input type="text" placeholder="Event Detail" className="modal_input" onChange={this.onDetailChange} />
                     </div>
                     <div className="actions">
-                        <button className="modal_btn">Save</button>
+                        <button className="modal_btn" onClick={this.onSubmit}>Save</button>
                     </div>
                 </div>
             </Popup>
-        );
+            )
+        } else if(this.props.openDetail) {
+            return (
+                <Popup
+                open={this.props.closeModalDetail}
+                onClose={this.closeModal}
+            >
+                    <div className="modal">
+                        <a className="close" onClick={this.props.closeModalDetail}>
+                            &times;
+                        </a>
+                        <div className="header">{this.props.slotInfo ? this.props.slotInfo.title : ''}</div>
+                        <div className="content">
+                            <h4>Date: {this.props.slotInfo ? this.props.slotInfo.start.toLocaleDateString() : ''}</h4>
+                            <h4>Detail: {this.props.slotInfo ? this.props.slotInfo.detail : ''}</h4>
+                        </div>
+                    </div>
+                </Popup>
+            )
+        } else {
+            return false
+        }
     }
 }
 
